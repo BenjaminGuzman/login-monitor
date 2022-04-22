5defb7fc653b30da64c582489fefda0852486cf4
Benjamín Guzmán <bg@benjaminguzman.dev>
2022-04-21 22:01:35 -0500

    Improvements to config options Fixed bug in main when reading config file Updated README.md

---
+++ email-config.go
@@ -0,0 +1,12 @@
+package main
+
+type EmailConfig struct {
+	Sender         Entity   `json:"sender"`
+	Recipient      Entity   `json:"recipient"`
+	Cc             []Entity `json:"cc"`
+	Subject        string   `json:"subject"`
+	TextMessage    string   `json:"textMessage"`
+	HTMLMessage    string   `json:"htmlMessage"`
+	Attachments    []string `json:"attachments"`
+	SenderPassFile string   `json:"senderPassFile"` // path to the sender's private key passphrase (required if the message is signed)
+}
--- helpers_test.go
+++ helpers_test.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"os"
 	"reflect"
 	"testing"
+	"time"
 )
 
 func TestWrap(t *testing.T) {
@@ -24,6 +26,36 @@ func TestWrap(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := Wrap(tt.args.src, tt.args.maxLen, tt.args.sep); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("Wrap() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestReplacePlaceholders(t *testing.T) {
+	timeTemplate := "hola %t02 Jan 06t% mundo %t Monday, 02-Jan-06 t% bye"
+	fileTemplate := "hola %f helpers_test.go f% mundo. %fhelpers_test.gof% bye"
+	expectedTimeReplacement := "hola " + time.Now().Format("02 Jan 06") + " mundo " + time.Now().Format("Monday, 02-Jan-06") + " bye"
+
+	expectedFileReplacement := "hola "
+	f, _ := os.ReadFile("helpers_test.go")
+	expectedFileReplacement += string(f) + " mundo. " + string(f) + " bye"
+
+	type args struct {
+		str string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{"Testing replacement of time", args{timeTemplate}, expectedTimeReplacement},
+		{"Testing file replacement", args{fileTemplate}, expectedFileReplacement},
+		{"Testing both replacements", args{timeTemplate + " " + fileTemplate}, expectedTimeReplacement + " " + expectedFileReplacement},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := ReplacePlaceholders(tt.args.str); got != tt.want {
+				t.Errorf("ReplacePlaceholders() = %v, want %v", got, tt.want)
 			}
 		})
 	}
